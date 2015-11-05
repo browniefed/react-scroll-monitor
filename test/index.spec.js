e59826b488e174598cb32c4ca2aed651906f8645
Jason Brown <browniefed@gmail.com>
2015-11-05 10:03:50 -0800

    Add some tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,66 @@
 import expect from 'expect';
-import { add } from '../src';
+var Handler = require('../src/handler');
+var getUniqueId = require('../src/utils/getUniqueId');
 
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+var id = 1;
+
+
+describe('ScrollSource', function() {
+
+})
+
+describe('ScrollListener', function() {
+  
+})
+
+describe('Unique id', function() {
+  it('should return a new id when called', function() {
+    var startId = getUniqueId();
+    expect(startId).toNotEqual(getUniqueId());
+  })
+})
+
+describe('Handler', () => {
+  beforeEach(function() {
+    id += 1;
   });
+  
+  describe('update listener', function() {
+    it ('should register update listener with an id', function() {
+      Handler.registerUpdateListner(id, function() {});
+      expect(Handler.getUpdateListeners(id).length).toBe(1);
+    });
+
+    it ('should return an object with a remove function', function() {
+      var removeListener = Handler.registerUpdateListner(id, function() {});
+      expect(removeListener.remove).toBeA('function')
+    });
+
+    it('should remove the listener when remove is called', function() {
+      var removeListener = Handler.registerUpdateListner(id, function() {});
+      expect(Handler.getUpdateListeners(id).length).toBe(1);
+      removeListener.remove();
+      expect(Handler.getUpdateListeners(id).length).toBe(0);
+    });
+  });
+
+  describe('scroll listener', function() {
+    it ('should register update listener with an id', function() {
+      Handler.registerScrollListener(id, function() {});
+      expect(Handler.getScrollListeners(id).length).toBe(1);
+    });
+
+    it ('should return an object with a remove function', function() {
+      var removeListener = Handler.registerScrollListener(id, function() {});
+      expect(removeListener.remove).toBeA('function')
+    });
+
+    it('should remove the listener when remove is called', function() {
+      var removeListener = Handler.registerScrollListener(id, function() {});
+      expect(Handler.getScrollListeners(id).length).toBe(1);
+      removeListener.remove();
+      expect(Handler.getScrollListeners(id).length).toBe(0);
+    });  
+  });
+
 });
